c976b810510525948446b14037bb8dffd474fbc1
stahhl <arvostahl@gmail.com>
2019-08-14 09:01:07 +0200

    KeyboardController

---
+++ Assets/Scripts/Controllers/KeyboardController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    //Fields
+    private PlayerController pC;
+    private float moveSpeed;
+    private float roatateSpeed;
+    private float rotZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pC = GetComponentInParent<PlayerController>();
+        moveSpeed = 5f;
+        roatateSpeed = 35f;
+        rotZ = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGeneralButtons();
+        UpdateCameraButtons();
+        UpdateGameplayButtons();
+    }
+    void UpdateGeneralButtons()
+    {
+
+    }
+    void UpdateCameraButtons()
+    {
+        //Move forward
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            pC.MainCameraObj.transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
+        }
+        //Move backward
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            pC.MainCameraObj.transform.Translate(new Vector3(0, -moveSpeed * Time.deltaTime, 0));
+        }
+        //Move right
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            pC.MainCameraObj.transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        }
+        //Move left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            pC.MainCameraObj.transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+        }
+        //Rotate left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotZ += Time.deltaTime * roatateSpeed;
+            Quaternion localRotation = Quaternion.Euler(0.0f, 0.0f, rotZ);
+            pC.MainCameraObj.transform.rotation = localRotation;
+        }
+        //Rotate right
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotZ -= Time.deltaTime * roatateSpeed;
+            Quaternion localRotation = Quaternion.Euler(0.0f, 0.0f, rotZ);
+            pC.MainCameraObj.transform.rotation = localRotation;
+        }
+        //Reset position
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
+            pC.ResetCameraPos();
+            rotZ = 0f;
+        }
+    }
+    void UpdateGameplayButtons()
+    {
+
+    }
+}
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Properties
     public GameObject MainCameraObj { get; private set; }
     public Camera MainCamera { get; private set; }
 
+    //Controllers - controllers are only cached in PlayerController
+    //All other classes calls controllers through PlayerController
+    //example: playerController.mouseController
     public MouseController mouseController { get; private set; }
     public UiController uiController { get; private set; }
 
+    //SerializeField
+    //Set in inspector
     [SerializeField]
     private GameObject uiControllerObj;
+
+    //Fields
+    //Set in code
+    private Vector3 originalCameraPos;
+    private Quaternion originalCameraRot;
 
     // Start is called before the first frame update
     void Start()
     {
         MainCamera = Camera.main;
         MainCameraObj = MainCamera.transform.parent.gameObject;
+
+        originalCameraPos = MainCameraObj.transform.position;
+        originalCameraRot = MainCameraObj.transform.rotation;
 
         mouseController = this.GetComponent<MouseController>();
         uiController = uiControllerObj.GetComponent<UiController>();
@@ -33,4 +47,10 @@ public class PlayerController : MonoBehaviour
     {
         uiController.NewAreaHighlighted(newArea);
     }
+    public void ResetCameraPos()
+    {
+        Debug.Log("ResetCameraPos: " + MainCameraObj.transform.position + " " + originalCameraPos);
+        MainCameraObj.transform.position = originalCameraPos;
+        MainCameraObj.transform.rotation = originalCameraRot;
+    } 
 }
